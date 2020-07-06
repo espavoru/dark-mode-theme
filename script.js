4f9576ed6c379cc462eedbf76343c0e301bf34b8
Oleg <espavoru@gmail.com>
2020-07-06 21:36:43 +0300

    Refactor pt2

--- script.js
+++ script.js
@@ -31,36 +31,18 @@ function toggleDarkLightMode(isDark) {
   isDark ? imageMode("dark") : imageMode("light");
 }
 
-// Dark Mode Styles
-function darkMode() {
-  nav.style.backgroundColor = "rgb(0 0 0 / 50%";
-  textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
-  toggleIcon.children[0].textContent = "Dark Mode";
-  toggleIcon.children[1].classList.replace("fa-sun", "fa-moon");
-  imageMode("dark");
-}
-
-// Light Mode Styles
-function lightMode() {
-  nav.style.backgroundColor = "rgb(255 255 255 / 50%)";
-  textBox.style.backgroundColor = "rgb(0 0 0 / 50%";
-  toggleIcon.children[0].textContent = "Light Mode";
-  toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
-  imageMode("light");
-}
-
 // Switch Theme Dynamically
 function switchTheme(event) {
   if (event.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
     localStorage.setItem("theme", "dark");
 
-    darkMode();
+    toggleDarkLightMode(true);
   } else {
     document.documentElement.setAttribute("data-theme", "light");
     localStorage.setItem("theme", "light");
 
-    lightMode();
+    toggleDarkLightMode(false);
   }
 }
 
@@ -74,6 +56,6 @@ if (currentTheme) {
 
   if (currentTheme === "dark") {
     toggleSwitch.checked = true;
-    darkMode();
+    toggleDarkLightMode(true);
   }
 }
